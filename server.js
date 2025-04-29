1a0a0a2bef248c4ff2027acc4b29f163860f31be
crmkt <crmarketing.carol@gmail.com>
2025-04-29 11:18:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-const wppconnect = require('@wppconnect-team/wppconnect');
-const axios = require('axios');
 
-wppconnect.create({
-  session: 'carol-session',
-  catchQR: (base64Qr, asciiQR) => {
-    console.log('QR RECEBIDO:', asciiQR);
-  },
-  headless: false,          
-  useChrome: true,           
-  devtools: false,
-  browserArgs: [''],
-  puppeteerOptions: {},
-  disableWelcome: true,
-  updatesLog: true
-}).then((client) => {
-  client.onMessage(async (message) => {
-    if (message.isGroupMsg && !message.fromMe) {
-      console.log('📥 Mensagem recebida:', message.body);
-
-      // Envia pro webhook do n8n
-      try {
-        const response = await axios.post('await axios.post('https://n8n-production-9d818.up.railway.app/webhook/teste-mensagem', {
+// Envia pro webhook do n8n
+try {
+        await axios.post('https://n8n-production-9d818.up.railway.app/webhook/editar-mensagem
 ', {
+          text: message.body,
+          from: message.from,
+          name: message.sender?.pushname || '',
+        });
+        const response = await axios.post('https://n8n-production-9d818.up.railway.app/webhook/editar-mensagem', {
   text: message.body,
   from: message.from,
   name: message.sender?.pushname || '',
@@ -36,9 +22,6 @@ if (response.data && response.data.mensagem) {
   console.log('⚠️ Resposta do n8n não veio como esperado:', response.data);
 }
 
-      } catch (err) {
-        console.error('❌ Erro ao enviar pro n8n:', err.message);
-      }
-    }
-  });
-});
+} catch (err) {
+console.error('❌ Erro ao enviar pro n8n:', err.message);
+}
